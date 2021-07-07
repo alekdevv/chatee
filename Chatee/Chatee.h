2da1605413748e68c72bc6a3f94e91e5c7bb35e9
Nikola Aleksendric <nikola.aleksendric@teletrader.com>
2021-07-07 12:22:14 +0200

    Project initialized

---
+++ Chatee/Chatee.h
@@ -0,0 +1,18 @@
+//
+//  Chatee.h
+//  Chatee
+//
+//  Created by Nikola Aleksendric on 7/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Chatee.
+FOUNDATION_EXPORT double ChateeVersionNumber;
+
+//! Project version string for Chatee.
+FOUNDATION_EXPORT const unsigned char ChateeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chatee/PublicHeader.h>
+
+
