0511f7e3093ded963e83dc95939df58dc496bef3
Nikola Aleksendric <alekdevv@gmail.com>
2021-07-14 17:59:47 +0200

    LoggedIn Env Var added

--- Demo/ChateeDemoApp/Coordinator/AppCoordinator.swift
+++ Demo/ChateeDemoApp/Coordinator/AppCoordinator.swift
@@ -10,12 +10,13 @@ import UIKit
 final class AppCoordinator: Coordinator {
     
     var viewController: UIViewController? {
-        return Account.shared.isLoggedIn ? self.tabBarController : self.loginViewController
+        let alreadyLoggedInTestVar = Bool(ProcessInfo.processInfo.environment["LoggedIn"] ?? "false") ?? false
+        
+        return alreadyLoggedInTestVar ? self.tabBarController : Account.shared.isLoggedIn ? self.tabBarController : self.loginViewController
     }
     
     private lazy var loginViewController: UIViewController = {
         let viewController = LoginViewController()
-        viewController.view.backgroundColor = .yellow
         
         return viewController
     }()
@@ -23,21 +24,22 @@ final class AppCoordinator: Coordinator {
     private lazy var tabBarController: UITabBarController = {
         let tabBarController = UITabBarController()
         tabBarController.setViewControllers(self.tabViewControllers, animated: true)
+        tabBarController.tabBar.tintColor = .red
         
         return tabBarController
     }()
     
     private lazy var tabViewControllers: [UIViewController] = {
         let chatsViewController = ChatsViewController()
+        chatsViewController.tabBarItem = UITabBarItem(title: "Chats", image: UIImage(named: "chats-icon")!, tag: 0)
+        
         let contactsViewController = ContactsViewController()
+        contactsViewController.tabBarItem = UITabBarItem(title: "Contacts", image: UIImage(named: "contacts-icon")!, tag: 0)
+        
         let settingsViewController = SettingsViewController()
+        settingsViewController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "settings-icon")!, tag: 0)
         
         return [chatsViewController, contactsViewController, settingsViewController]
     }()
-    
-    
-    init() {
-        
-    }
     
 }
